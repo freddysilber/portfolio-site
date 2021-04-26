baefc16ba64f642274cbd2ccb59df586ceb5da3b
freddysilber <freddy.silber@gmail.com>
2021-04-25 18:47:48 -0600

    Updates

---
+++ component---src-pages-404-tsx-feb35db7cc5a505dc85a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"i6+/":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),c=n("Nhdc");t.default=function(){return l.a.createElement(c.c,null,l.a.createElement(c.e,{title:"404: Not found"}),l.a.createElement("div",{className:"has-text-centered"},l.a.createElement("h1",null,"404: Not Found"),l.a.createElement("hr",null),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-feb35db7cc5a505dc85a.js.map
