0b3ed17e7f26768a31ceb3fe0d8504af25f0e444
freddysilber <freddy.silber@gmail.com>
2021-05-02 13:04:49 -0600

    Updates

---
+++ component---src-pages-resume-tsx-6996d7433337dad95713.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{UTyF:function(e,n,t){"use strict";t.r(n),t.d(n,"default",(function(){return r}));var c=t("q1tI"),u=t.n(c),a=t("Nhdc");function r(){return u.a.createElement(a.c,null,u.a.createElement(a.f,{title:"Resume"}),u.a.createElement(a.e,null))}}}]);
+//# sourceMappingURL=component---src-pages-resume-tsx-6996d7433337dad95713.js.map
